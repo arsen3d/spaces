42ced5b60bae1541b5522e4dcc993612cf8d4f0c
Arsen Yeremin <arsen3d@gmail.com>
2024-10-07 13:36:57 -0700

    update

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     entry: './src/index.js',
     output: {
-      path: path.resolve(__dirname, 'dist/spaces'),
+      path: path.resolve(__dirname, 'dist/spaces/build'),
       filename: 'bundle.js',
       publicPath: '/spaces/build'
     },
